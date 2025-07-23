59f711025bdc42d22a7a9146b7b03ae4f34fb844
surya <kommojusurya61@gmail.com>
2025-07-23 23:12:09 +0530

    Initialised Dashboard,signup,signin

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
 
 import './App.css'
-
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Dashboard from './components/Dashboard'
+import SignUp from './components/SignUp'
+import SignIn from './components/SignIn'
 function App() {
   
 
   return (
     <>
       <div>
-        Second brain FrontEnd
+        <BrowserRouter>
+          <Routes>
+            <Route path='/dashboard' element={<Dashboard/>} />
+            <Route path='/signup' element={<SignUp/>} />
+            <Route path='/signin' element={<SignIn/>} />
+          </Routes>
+        </BrowserRouter>
       </div>
     </>
   )
